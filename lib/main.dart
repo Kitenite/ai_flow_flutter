8baeee05b03b85a4f08c4304ac7eb37f3714ae6d
Kiet Ho <hoakiet98@gmail.com>
2023-01-26 20:08:06 -0600

    Saving progress camera

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:ai_flow/home.dart';
+import 'package:flutter/material.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Flutter Camera Demo',
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
